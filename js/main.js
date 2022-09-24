aa1af62a6fc64745101a0d1d0da74e1d7a08a7d3
buwjt22 <buwjt22@gmail.com>
2022-09-25 04:09:47 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// TOPへ戻る 
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
+
+scrollTop('button');
+
+// ﾓｰﾀﾞﾙｳｨﾝﾄﾞｳ
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = document.getElementById('modal');
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
+    modal.style.display = 'none';
+  }
+});
