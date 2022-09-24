c2c7bbded5f9aa8151b53cc1575af2b7be7cbf86
buwjt22 <buwjt22@gmail.com>
2022-09-25 04:25:23 +0900

    second-commit

--- js/main.js
+++ js/main.js
@@ -27,3 +27,17 @@ window.addEventListener('click', (e) => {
     modal.style.display = 'none';
   }
 });
+
+// ﾊﾝﾊﾞｰｶﾞｰﾒﾆｭｰ
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+});
+
+// ﾄﾞﾛﾜｰﾒﾆｭｰ
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
